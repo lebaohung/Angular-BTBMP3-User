c55d3beaed49930d29e5155d80181babbff02a02
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-07-23 10:17:27 +0700

    make my playlist page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import {RegistrationComponent} from './component/registration/registration.compo
 import {LoginComponent} from './component/login/login.component';
 import { NewSongComponent } from './component/new-song/new-song.component';
 import {PlaylistComponent} from './playlists/playlist/playlist.component';
+import {MyPlaylistComponent} from './playlists/my-playlist/my-playlist.component';
 
 const routes: Routes = [
   { path: '', component: MainViewComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'newSong', component: NewSongComponent}
+  { path: 'newSong', component: NewSongComponent},
   { path: 'playlist', component: PlaylistComponent},
+  { path: 'myPlaylist', component: MyPlaylistComponent},
 ];
 
 @NgModule({
