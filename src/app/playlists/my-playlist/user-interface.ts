069a3fced8c4f82e0112c42d5f7364af65fcf8e8
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-07-24 23:15:26 +0700

    add userInterface

---
+++ src/app/playlists/my-playlist/user-interface.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+  id: number;
+  username: string;
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+  status: boolean;
+  birthday: Date;
+  createDate: Date;
+  roles: [];
+}
