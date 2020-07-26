ce37917a320cac40abcefc87eaee9e0636232054
Vu Dai Duong <daiduongutc97@gmail.com>
2020-07-27 03:36:16 +0700

    Complete View Top Newest Song Complete View Top Viewed Song

--- src/app/component/main-view/main-view.component.ts
+++ src/app/component/main-view/main-view.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {Iplaylist} from '../../playlists/create-playlist/playlist';
-import {PlaylistService} from '../../service/playlists/playlist.service';
+import {Component, OnInit} from '@angular/core';
+import {SongsService} from '../../service/song/songs.service';
+import {Song} from '../../model/song';
+import {Singer} from '../../model/singer';
+import {SingerAndSong} from '../../model/singerAndSong';
+import {SingerService} from '../../service/singer/singer.service';
 
 @Component({
   selector: 'app-main-view',
@@ -10,21 +13,59 @@ import {PlaylistService} from '../../service/playlists/playlist.service';
 export class MainViewComponent implements OnInit {
 
 
-  constructor(private playlistService: PlaylistService) { }
-  playListTopView: Iplaylist[] = [];
-  playListTopDate: Iplaylist[] = [];
+  constructor(private songsService: SongsService) {
+  }
+
+  songsTopView: Song[] = [];
+  songsTopDate: Song[] = [];
+  selectedSongTopViewId = 0;
+  selectedSingerTopView: Singer = {
+    id: 0,
+    name: '',
+    create_date: ''
+  };
+  selectedSongTopDateId = 0;
+  selectedSingerTopDate: Singer = {
+    id: 0,
+    name: '',
+    create_date: ''
+  };
+  singerAndSongTopDate: SingerAndSong[] = [];
+  singerAndSongTopView: SingerAndSong[] = [];
 
   ngOnInit(): void {
-    this.playlistService.getTopDate().subscribe((result) => {
-      this.playListTopDate = result;
-      console.log(result);
+    this.songsService.getTopDate().subscribe((result) => {
+      this.songsTopDate = result;
+      for (let i = 0; i < this.songsTopDate.length; i++) {
+        this.selectedSongTopDateId = this.songsTopDate[i].id;
+        this.songsService.getSingerOfThisSong(this.selectedSongTopDateId).subscribe(value => {
+          this.selectedSingerTopDate = value;
+          this.singerAndSongTopDate.push(
+            {
+              song: this.songsTopDate[i],
+              singer: this.selectedSingerTopDate
+            });
+        });
+      }
+      console.log(this.singerAndSongTopDate);
     }, error => {
       console.log(error);
     });
 
-    this.playlistService.getTopView().subscribe((result) => {
-      this.playListTopView = result;
-      console.log(result);
+    this.songsService.getTopView().subscribe((result) => {
+      this.songsTopView = result;
+      for (let i = 0; i < this.songsTopView.length; i++) {
+        this.selectedSongTopViewId = this.songsTopView[i].id;
+        this.songsService.getSingerOfThisSong(this.selectedSongTopViewId).subscribe(value => {
+          this.selectedSingerTopView = value;
+          this.singerAndSongTopView.push(
+            {
+              song: this.songsTopView[i],
+              singer: this.selectedSingerTopView
+            });
+        });
+      }
+      console.log(this.singerAndSongTopView);
     }, error => {
       console.log(error);
     });
