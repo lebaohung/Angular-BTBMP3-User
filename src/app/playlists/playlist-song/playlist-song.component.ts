f7de56bc3d34de45299ddf50435abe81b31ed3a9
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-07-27 23:31:49 +0700

    add TS comment playlist

--- src/app/playlists/playlist-song/playlist-song.component.ts
+++ src/app/playlists/playlist-song/playlist-song.component.ts
@@ -2,6 +2,10 @@ import {Component, Input, OnInit} from '@angular/core';
 import {PlaylistService} from '../../service/playlists/playlist.service';
 import {Song} from '../../model/song';
 import {ActivatedRoute} from '@angular/router';
+import {ICommentPlaylist} from '../../model/comment-playlist';
+import {Users} from '../../model/users';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Iplaylist} from '../create-playlist/playlist';
 
 @Component({
   selector: 'app-playlist-song',
@@ -18,7 +22,16 @@ export class PlaylistSongComponent implements OnInit {
 
   idPlayList: number;
 
+  commentPlaylist: ICommentPlaylist[] = [];
+  comment: FormGroup;
+  playlist: Iplaylist;
+
   ngOnInit(): void {
+    this.onload();
+    this.playlistService.shouldRefresh.subscribe(value => this.onload());
+  }
+
+  onload(): void {
     this.playlistService.shouldRefresh.subscribe(value => {
       this.activatedRoute.params.subscribe(result => this.idPlayList = result.id);
       this.playlistService.playSong(this.idPlayList).subscribe(value1 => this.songList = value1);
@@ -26,6 +39,17 @@ export class PlaylistSongComponent implements OnInit {
     this.activatedRoute.params.subscribe(result => this.idPlayList = result.id);
     this.playlistService.playSong(this.idPlayList).subscribe(value => this.songList = value);
 
+    this.playlistService.getConment(this.idPlayList).subscribe(value => this.commentPlaylist = value);
+
+    this.comment = new FormGroup({
+      id: new FormControl(''),
+      content: new FormControl('', [Validators.required]),
+      date: new FormControl(''),
+      playlist: new FormControl(''),
+      user: new FormControl(''),
+    });
+
+    this.playlistService.getById(this.idPlayList).subscribe(value => this.playlist = value);
   }
 
   delete(id: number): void {
@@ -35,5 +59,12 @@ export class PlaylistSongComponent implements OnInit {
 
   deleteSong(): void {
     this.playlistService.deleteSongPlaylist(this.idPlaylistSong).subscribe(value => {this.playlistService.shouldRefresh.next(); });
+  }
+
+
+  addCommentPlaylist(): void {
+    this.comment.get('user').setValue(JSON.parse(localStorage.getItem('user')));
+    this.comment.get('playlist').setValue(this.playlist);
+    this.playlistService.addComment(this.comment.value).subscribe(value => this.playlistService.shouldRefresh.next());
   }
 }
