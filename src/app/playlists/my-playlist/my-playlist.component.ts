d8b270d5323eb6db7eed35c0596b51945fee46e5
Vu Dai Duong <daiduongutc97@gmail.com>
2020-07-31 09:11:43 +0700

    edit image size Mainview edit link firebase

--- src/app/playlists/my-playlist/my-playlist.component.ts
+++ src/app/playlists/my-playlist/my-playlist.component.ts
@@ -7,8 +7,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {FileUpload} from '../../model/file-upload';
 import {UploadFileService} from '../../service/upload-file/upload-file.service';
 
-const FRONT_LINK = 'https://firebasestorage.googleapis.com/v0/b/project-module-5.appspot.com/o/uploads%2F';
-const BACK_LINK = '?alt=media&token=fad94b03-0cbe-49a5-b06f-4c2284bc4bd8';
+const FRONT_LINK = 'https://firebasestorage.googleapis.com/v0/b/btbmp3.appspot.com/o/uploads%2F';
+const BACK_LINK = '?alt=media&token=8931cbe0-eac5-4071-986d-b99e417ca6fc';
 
 @Component({
   selector: 'app-my-playlist',
